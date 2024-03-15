b410c3795e9a94334818f352f508bea4f2e7dcaa
TGMM <tgmm.1000@gmail.com>
2024-03-15 10:27:53 -0700

    Fixed formatting for all files

--- Server/src/actionHandlers.ts
+++ Server/src/actionHandlers.ts
@@ -1,58 +1,58 @@
-import type Client from "./Client";
-import Lobby from "./Lobby";
+import type Client from './Client'
+import Lobby from './Lobby'
 import type {
-  ActionCreateLobby,
-  ActionFnArgs,
-  ActionHandlers,
-  ActionJoinLobby,
-  ActionUsername,
-} from "./actions";
-import { serializeAction } from "./main";
+	ActionCreateLobby,
+	ActionFnArgs,
+	ActionHandlers,
+	ActionJoinLobby,
+	ActionUsername,
+} from './actions'
+import { serializeAction } from './main'
 
 const usernameAction = (
-  { username }: ActionFnArgs<ActionUsername>,
-  client: Client,
+	{ username }: ActionFnArgs<ActionUsername>,
+	client: Client,
 ) => {
-  client.setUsername(username);
-};
+	client.setUsername(username)
+}
 
 const createLobbyAction = (
-  { gameMode }: ActionFnArgs<ActionCreateLobby>,
-  client: Client,
+	{ gameMode }: ActionFnArgs<ActionCreateLobby>,
+	client: Client,
 ) => {
-  new Lobby(client);
-};
+	new Lobby(client)
+}
 
 const joinLobbyAction = (
-  { code }: ActionFnArgs<ActionJoinLobby>,
-  client: Client,
+	{ code }: ActionFnArgs<ActionJoinLobby>,
+	client: Client,
 ) => {
-  const newLobby = Lobby.get(code);
-  if (!newLobby) {
-    client.send(
-      serializeAction({
-        action: "error",
-        message: "Lobby does not exist.",
-      }),
-    );
-    return;
-  }
-  newLobby.join(client);
-};
+	const newLobby = Lobby.get(code)
+	if (!newLobby) {
+		client.send(
+			serializeAction({
+				action: 'error',
+				message: 'Lobby does not exist.',
+			}),
+		)
+		return
+	}
+	newLobby.join(client)
+}
 
 const leaveLobbyAction = (client: Client) => {
-  client.lobby?.leave(client);
-};
+	client.lobby?.leave(client)
+}
 
 const lobbyInfoAction = (client: Client) => {
-  client.lobby?.broadcast();
-};
+	client.lobby?.broadcast()
+}
 
 // Declared partial for now untill all action handlers are defined
 export const actionHandlers: Partial<ActionHandlers> = {
-  username: usernameAction,
-  createLobby: createLobbyAction,
-  joinLobby: joinLobbyAction,
-  lobbyInfo: lobbyInfoAction,
-  leaveLobby: leaveLobbyAction,
-};
+	username: usernameAction,
+	createLobby: createLobbyAction,
+	joinLobby: joinLobbyAction,
+	lobbyInfo: lobbyInfoAction,
+	leaveLobby: leaveLobbyAction,
+}
--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,94 +1,93 @@
-import net from "node:net";
-import Client from "./Client";
-import Lobby from "./Lobby";
-import type { ActionClientToServer, ActionServerToClient } from "./actions";
-import { actionHandlers } from "./actionHandlers";
+import net from 'node:net'
+import Client from './Client'
+import { actionHandlers } from './actionHandlers'
+import type { ActionClientToServer, ActionServerToClient } from './actions'
 
-const PORT = 8080;
+const PORT = 8080
 
 // biome-ignore lint/suspicious/noExplicitAny: Object is parsed from string
 const stringToJson = (str: string): any => {
-  // biome-ignore lint/suspicious/noExplicitAny: Object is parsed from string
-  const obj: any = {};
-  for (const part of str.split(",")) {
-    const [key, value] = part.split(":");
-    obj[key] = value;
-  }
-  return obj;
-};
+	// biome-ignore lint/suspicious/noExplicitAny: Object is parsed from string
+	const obj: any = {}
+	for (const part of str.split(',')) {
+		const [key, value] = part.split(':')
+		obj[key] = value
+	}
+	return obj
+}
 
 /** Serializes an action for transmission to the client */
 export const serializeAction = (action: ActionServerToClient): string => {
-  const entries = Object.entries(action);
-  const parts = entries
-    .filter(([_key, value]) => value !== undefined && value !== null)
-    .map(([key, value]) => `${key}:${value}`);
-  return parts.join(",");
-};
+	const entries = Object.entries(action)
+	const parts = entries
+		.filter(([_key, value]) => value !== undefined && value !== null)
+		.map(([key, value]) => `${key}:${value}`)
+	return parts.join(',')
+}
 
 const sendToSocket = (socket: net.Socket) => (data: string) => {
-  if (!socket) {
-    return;
-  }
-  socket.write(`${data}\n`);
-};
+	if (!socket) {
+		return
+	}
+	socket.write(`${data}\n`)
+}
 
 const server = net.createServer((socket) => {
-  const client = new Client(sendToSocket(socket));
-  client.send(serializeAction({ action: "connected" }));
+	const client = new Client(sendToSocket(socket))
+	client.send(serializeAction({ action: 'connected' }))
 
-  socket.on("data", (data) => {
-    const messages = data.toString().split("\n");
+	socket.on('data', (data) => {
+		const messages = data.toString().split('\n')
 
-    for (const msg of messages) {
-      if (!msg) return;
-      try {
-        const message: ActionClientToServer = stringToJson(msg);
-        const { action, ...actionArgs } = message;
+		for (const msg of messages) {
+			if (!msg) return
+			try {
+				const message: ActionClientToServer = stringToJson(msg)
+				const { action, ...actionArgs } = message
 
-        // This only works for now, once we add more arguments
-        // we'll need to refactor this
-        // Maybe add a context type that includes everything
-        // connection related?
-        actionArgs
-          ? actionHandlers[action]?.(actionArgs, client)
-          : actionHandlers[action]?.(client);
-      } catch (error) {
-        const failedToParseError = "Failed to parse message";
-        console.error(failedToParseError, error);
-        client.send(
-          serializeAction({
-            action: "error",
-            message: failedToParseError,
-          }),
-        );
-      }
-    }
-  });
+				// This only works for now, once we add more arguments
+				// we'll need to refactor this
+				// Maybe add a context type that includes everything
+				// connection related?
+				actionArgs
+					? actionHandlers[action]?.(actionArgs, client)
+					: actionHandlers[action]?.(client)
+			} catch (error) {
+				const failedToParseError = 'Failed to parse message'
+				console.error(failedToParseError, error)
+				client.send(
+					serializeAction({
+						action: 'error',
+						message: failedToParseError,
+					}),
+				)
+			}
+		}
+	})
 
-  socket.on("end", () => {
-    actionHandlers.leaveLobby?.(client);
-  });
+	socket.on('end', () => {
+		actionHandlers.leaveLobby?.(client)
+	})
 
-  socket.on(
-    "error",
-    (
-      err: Error & {
-        errno: number;
-        code: string;
-        syscall: string;
-      },
-    ) => {
-      if (err.code === "ECONNRESET") {
-        console.warn("TCP connection reset by peer (client).");
-      } else {
-        console.error("An unexpected error occurred:", err);
-      }
-      actionHandlers.leaveLobby?.(client);
-    },
-  );
-});
+	socket.on(
+		'error',
+		(
+			err: Error & {
+				errno: number
+				code: string
+				syscall: string
+			},
+		) => {
+			if (err.code === 'ECONNRESET') {
+				console.warn('TCP connection reset by peer (client).')
+			} else {
+				console.error('An unexpected error occurred:', err)
+			}
+			actionHandlers.leaveLobby?.(client)
+		},
+	)
+})
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+server.listen(PORT, '0.0.0.0', () => {
+	console.log(`Server listening on port ${PORT}`)
+})
