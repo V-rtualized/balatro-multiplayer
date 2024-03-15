f2dca3cad915bdc95b3f870aa594989c17d26950
TGMM <tgmm.1000@gmail.com>
2024-03-14 18:28:56 -0700

    Moved the server to Typescript

--- Server/src/Client.ts
+++ Server/src/Client.ts
@@ -1,19 +1,27 @@
 import { v4 as uuidv4 } from 'uuid'
+import type Lobby from './Lobby'
+
+type SendFn = (data: string) => void
 
 class Client {
-  constructor(send) {
+  id: string
+  username: string
+  lobby: Lobby | null
+  send: SendFn
+
+  constructor(send: SendFn) {
     this.id = uuidv4()
     this.lobby = null
     this.username = 'Guest'
     this.send = send
   }
 
-  setUsername = (username) => {
+  setUsername = (username: string) => {
     this.username = username
     this.lobby?.broadcast()
   }
 
-  setLobby = (lobby) => {
+  setLobby = (lobby: Lobby | null) => {
     this.lobby = lobby
   }
 }
