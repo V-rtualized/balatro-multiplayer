b21ee6e4448be7a2d8d988f8188c961b387b6e73
Connor Mills <connor@virtualized.dev>
2024-05-27 02:40:37 -0700

    Removed colon from version missmatch warning

--- Server/src/actionHandlers.ts
+++ Server/src/actionHandlers.ts
@@ -229,7 +229,7 @@ const versionAction = (
 	client: Client,
 ) => {
 	if (version !== serverVersion) {
-		client.sendAction({ action: "error", message: `WARN: Server expecting version ${serverVersion}` });
+		client.sendAction({ action: "error", message: `[WARN] Server expecting version ${serverVersion}` });
 	}
 };
 
