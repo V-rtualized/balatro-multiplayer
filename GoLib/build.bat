7c606f31b384e23d2780622e0f9e7883a5b625de
Connor Mills <connor@virtualized.dev>
2024-12-27 10:37:52 -0800

    Removed unused modules

--- GoLib/build.bat
+++
@@ -1,4 +0,0 @@
-go build -o ./bin/multiplayer.a -buildmode=c-archive main.go
-cl /MD /c multiplayer.c /Fobin\ /link ./bin/multiplayer.a
-cl /LD /MD /Fe"./bin/multiplayer.dll" /Fo"./bin/multiplayer.obj" multiplayer.c /link /DEF:multiplayer.def ./bin/multiplayer.a /LIBPATH:C:\MinGW\lib
-PAUSE
--- GoLib/multiplayer.c
+++
@@ -1,2 +0,0 @@
-#include "./bin/multiplayer.h"
-#include "cgo.c"
