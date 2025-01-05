461c0e8429a6dbebb661a0c0988cc2e391db20c9
Connor Mills <connor@virtualized.dev>
2025-01-04 19:59:20 -0800

    Added compatibility with Cryptid

--- Multiplayer/Compatibility/Talisman.lua
+++ Multiplayer/Compatibility/Talisman.lua
@@ -1,6 +1,3 @@
-local to_big_ref = to_big or function(x, y)
+to_big = to_big or function(x, y)
 	return x
-end
-function to_big(x, y)
-	return to_big_ref(x, y)
 end
--- Multiplayer/Core.lua
+++ Multiplayer/Core.lua
@@ -20,6 +20,7 @@ local load_mp_file = G.MULTIPLAYER.load_mp_file
 load_mp_file("Utils.lua")
 
 load_mp_file("Compatibility/Talisman.lua")
+load_mp_file("Compatibility/Cryptid.lua")
 
 load_mp_file("Lobby.lua")
 load_mp_file("Networking/Action_Handlers.lua")
