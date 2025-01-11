3de0a93c91b4ec586c3d2049e33555673c01445c
Connor Mills <connor@virtualized.dev>
2025-01-10 20:35:36 -0800

    Added main menu effect

--- Multiplayer/Compatibility/Cryptid.lua
+++ Multiplayer/Compatibility/Cryptid.lua
@@ -7,20 +7,24 @@ if SMODS.Mods["Cryptid"] and SMODS.Mods["Cryptid"].can_load then
 	G.MULTIPLAYER.DECK.ban_card("c_cry_semicolon")
 	G.MULTIPLAYER.DECK.ban_card("c_cry_crash")
 	G.MULTIPLAYER.DECK.ban_card("c_cry_revert")
-end
 
-local defeat_ref = Blind.defeat
-function Blind:defeat(silent)
-	if self.config.blind.key == nil then
-		self.config.blind.key = "bl_nil"
+	local defeat_ref = Blind.defeat
+	function Blind:defeat(silent)
+		if self.config.blind.key == nil then
+			self.config.blind.key = "bl_nil"
+		end
+		defeat_ref(self, silent)
 	end
-	defeat_ref(self, silent)
-end
 
-local save_run_ref = save_run
-function save_run()
-	if G.F_NO_SAVING then
-		return
+	local save_run_ref = save_run
+	function save_run()
+		if G.F_NO_SAVING then
+			return
+		end
+		save_run_ref()
 	end
-	save_run_ref()
+
+	function wheel_of_fortune_the_title_card()
+		return true
+	end
 end
