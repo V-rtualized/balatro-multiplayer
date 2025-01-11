6dbcb8e260cd4dcfc282c3758cdce4a368b459e5
Connor Mills <connor@virtualized.dev>
2025-01-11 13:56:46 -0800

    Banned analog in Cryptid

--- Multiplayer/Compatibility/Cryptid.lua
+++ Multiplayer/Compatibility/Cryptid.lua
@@ -7,6 +7,7 @@ if SMODS.Mods["Cryptid"] and SMODS.Mods["Cryptid"].can_load then
 	G.MULTIPLAYER.DECK.ban_card("c_cry_semicolon")
 	G.MULTIPLAYER.DECK.ban_card("c_cry_crash")
 	G.MULTIPLAYER.DECK.ban_card("c_cry_revert")
+	G.MULTIPLAYER.DECK.ban_card("c_cry_analog")
 
 	local defeat_ref = Blind.defeat
 	function Blind:defeat(silent)
