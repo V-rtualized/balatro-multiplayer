c88fc86882d1c2c9ebd33b09e829ea9f864ee756
Connor Mills <connor@virtualized.dev>
2025-01-12 02:13:43 -0800

    Fixed the end round injection

--- Multiplayer/UI/Game_UI.lua
+++ Multiplayer/UI/Game_UI.lua
@@ -1022,213 +1022,204 @@ function Game:update_new_round(dt)
 end
 
 function G.MULTIPLAYER.end_round()
+	G.RESET_BLIND_STATES = true
+	G.RESET_JIGGLES = true
+	for i = 1, #G.jokers.cards do
+		local eval = nil
+		eval = G.jokers.cards[i]:calculate_joker({ end_of_round = true, game_over = game_over })
+		if eval then
+			card_eval_status_text(G.jokers.cards[i], "jokers", nil, nil, nil, eval)
+		end
+	end
+	G.GAME.unused_discards = (G.GAME.unused_discards or 0) + G.GAME.current_round.discards_left
+	if G.GAME.blind and G.GAME.blind.config.blind then
+		discover_card(G.GAME.blind.config.blind)
+	end
+
+	if G.GAME.blind:get_type() == "Boss" then
+		local _handname, _played, _order = "High Card", -1, 100
+		for k, v in pairs(G.GAME.hands) do
+			if v.played > _played or (v.played == _played and _order > v.order) then
+				_played = v.played
+				_handname = k
+			end
+		end
+		G.GAME.current_round.most_played_poker_hand = _handname
+	end
+
+	if G.GAME.blind:get_type() == "Boss" and not G.GAME.seeded and not G.GAME.challenge then
+		G.GAME.current_boss_streak = G.GAME.current_boss_streak + 1
+		check_and_set_high_score("boss_streak", G.GAME.current_boss_streak)
+	end
+
+	if G.GAME.current_round.hands_played == 1 then
+		inc_career_stat("c_single_hand_round_streak", 1)
+	else
+		if not G.GAME.seeded and not G.GAME.challenge then
+			G.PROFILES[G.SETTINGS.profile].career_stats.c_single_hand_round_streak = 0
+			G:save_settings()
+		end
+	end
+
+	check_for_unlock({ type = "round_win" })
+	set_joker_usage()
+	for i = 1, #G.hand.cards do
+		--Check for hand doubling
+		local reps = { 1 }
+		local j = 1
+		while j <= #reps do
+			local percent = (i - 0.999) / (#G.hand.cards - 0.998) + (j - 1) * 0.1
+			if reps[j] ~= 1 then
+				card_eval_status_text(
+					(reps[j].jokers or reps[j].seals).card,
+					"jokers",
+					nil,
+					nil,
+					nil,
+					(reps[j].jokers or reps[j].seals)
+				)
+			end
+
+			--calculate the hand effects
+			local effects = { G.hand.cards[i]:get_end_of_round_effect() }
+			for k = 1, #G.jokers.cards do
+				--calculate the joker individual card effects
+				local eval = G.jokers.cards[k]:calculate_joker({
+					cardarea = G.hand,
+					other_card = G.hand.cards[i],
+					individual = true,
+					end_of_round = true,
+				})
+				if eval then
+					table.insert(effects, eval)
+				end
+			end
+
+			if reps[j] == 1 then
+				--Check for hand doubling
+				--From Red seal
+				local eval = eval_card(
+					G.hand.cards[i],
+					{ end_of_round = true, cardarea = G.hand, repetition = true, repetition_only = true }
+				)
+				if next(eval) and (next(effects[1]) or #effects > 1) then
+					for h = 1, eval.seals.repetitions do
+						reps[#reps + 1] = eval
+					end
+				end
+
+				--from Jokers
+				for j = 1, #G.jokers.cards do
+					--calculate the joker effects
+					local eval = eval_card(G.jokers.cards[j], {
+						cardarea = G.hand,
+						other_card = G.hand.cards[i],
+						repetition = true,
+						end_of_round = true,
+						card_effects = effects,
+					})
+					if next(eval) then
+						for h = 1, eval.jokers.repetitions do
+							reps[#reps + 1] = eval
+						end
+					end
+				end
+			end
+
+			for ii = 1, #effects do
+				--if this effect came from a joker
+				if effects[ii].card then
+					G.E_MANAGER:add_event(Event({
+						trigger = "immediate",
+						func = function()
+							effects[ii].card:juice_up(0.7)
+							return true
+						end,
+					}))
+				end
+
+				--If dollars
+				if effects[ii].h_dollars then
+					ease_dollars(effects[ii].h_dollars)
+					card_eval_status_text(G.hand.cards[i], "dollars", effects[ii].h_dollars, percent)
+				end
+
+				--Any extras
+				if effects[ii].extra then
+					card_eval_status_text(G.hand.cards[i], "extra", nil, percent, nil, effects[ii].extra)
+				end
+			end
+			j = j + 1
+		end
+	end
+	delay(0.3)
+
+	G.FUNCS.draw_from_hand_to_discard()
+	if G.GAME.blind:get_type() == "Boss" then
+		G.GAME.voucher_restock = nil
+		if G.GAME.modifiers.set_eternal_ante and (G.GAME.round_resets.ante == G.GAME.modifiers.set_eternal_ante) then
+			for k, v in ipairs(G.jokers.cards) do
+				v:set_eternal(true)
+			end
+		end
+		if
+			G.GAME.modifiers.set_joker_slots_ante
+			and (G.GAME.round_resets.ante == G.GAME.modifiers.set_joker_slots_ante)
+		then
+			G.jokers.config.card_limit = 0
+		end
+		delay(0.4)
+		ease_ante(1)
+		delay(0.4)
+		check_for_unlock({ type = "ante_up", ante = G.GAME.round_resets.ante + 1 })
+	end
+	G.FUNCS.draw_from_discard_to_deck()
 	G.E_MANAGER:add_event(Event({
 		trigger = "after",
-		delay = 0.2,
+		delay = 0.3,
 		func = function()
-			G.RESET_BLIND_STATES = true
-			G.RESET_JIGGLES = true
-			for i = 1, #G.jokers.cards do
-				local eval = nil
-				eval = G.jokers.cards[i]:calculate_joker({ end_of_round = true, game_over = game_over })
-				if eval then
-					card_eval_status_text(G.jokers.cards[i], "jokers", nil, nil, nil, eval)
-				end
-			end
-			G.GAME.unused_discards = (G.GAME.unused_discards or 0) + G.GAME.current_round.discards_left
-			if G.GAME.blind and G.GAME.blind.config.blind then
-				discover_card(G.GAME.blind.config.blind)
-			end
+			G.STATE = G.STATES.ROUND_EVAL
+			G.STATE_COMPLETE = false
 
-			if G.GAME.blind:get_type() == "Boss" then
-				local _handname, _played, _order = "High Card", -1, 100
-				for k, v in pairs(G.GAME.hands) do
-					if v.played > _played or (v.played == _played and _order > v.order) then
-						_played = v.played
-						_handname = k
-					end
-				end
-				G.GAME.current_round.most_played_poker_hand = _handname
-			end
-
-			if G.GAME.blind:get_type() == "Boss" and not G.GAME.seeded and not G.GAME.challenge then
-				G.GAME.current_boss_streak = G.GAME.current_boss_streak + 1
-				check_and_set_high_score("boss_streak", G.GAME.current_boss_streak)
-			end
-
-			if G.GAME.current_round.hands_played == 1 then
-				inc_career_stat("c_single_hand_round_streak", 1)
+			if
+				G.GAME.round_resets.blind_states.Small ~= "Defeated"
+				and G.GAME.round_resets.blind_states.Small ~= "Skipped"
+			then
+				G.GAME.round_resets.blind_states.Small = "Defeated"
+			elseif
+				G.GAME.round_resets.blind_states.Big ~= "Defeated"
+				and G.GAME.round_resets.blind_states.Big ~= "Skipped"
+			then
+				G.GAME.round_resets.blind_states.Big = "Defeated"
 			else
-				if not G.GAME.seeded and not G.GAME.challenge then
-					G.PROFILES[G.SETTINGS.profile].career_stats.c_single_hand_round_streak = 0
-					G:save_settings()
+				G.GAME.current_round.voucher = get_next_voucher_key()
+				G.GAME.round_resets.blind_states.Boss = "Defeated"
+				for k, v in ipairs(G.playing_cards) do
+					v.ability.played_this_ante = nil
 				end
 			end
 
-			check_for_unlock({ type = "round_win" })
-			set_joker_usage()
-			for i = 1, #G.hand.cards do
-				--Check for hand doubling
-				local reps = { 1 }
-				local j = 1
-				while j <= #reps do
-					local percent = (i - 0.999) / (#G.hand.cards - 0.998) + (j - 1) * 0.1
-					if reps[j] ~= 1 then
-						card_eval_status_text(
-							(reps[j].jokers or reps[j].seals).card,
-							"jokers",
-							nil,
-							nil,
-							nil,
-							(reps[j].jokers or reps[j].seals)
-						)
-					end
-
-					--calculate the hand effects
-					local effects = { G.hand.cards[i]:get_end_of_round_effect() }
-					for k = 1, #G.jokers.cards do
-						--calculate the joker individual card effects
-						local eval = G.jokers.cards[k]:calculate_joker({
-							cardarea = G.hand,
-							other_card = G.hand.cards[i],
-							individual = true,
-							end_of_round = true,
-						})
-						if eval then
-							table.insert(effects, eval)
-						end
-					end
-
-					if reps[j] == 1 then
-						--Check for hand doubling
-						--From Red seal
-						local eval = eval_card(
-							G.hand.cards[i],
-							{ end_of_round = true, cardarea = G.hand, repetition = true, repetition_only = true }
-						)
-						if next(eval) and (next(effects[1]) or #effects > 1) then
-							for h = 1, eval.seals.repetitions do
-								reps[#reps + 1] = eval
-							end
-						end
-
-						--from Jokers
-						for j = 1, #G.jokers.cards do
-							--calculate the joker effects
-							local eval = eval_card(G.jokers.cards[j], {
-								cardarea = G.hand,
-								other_card = G.hand.cards[i],
-								repetition = true,
-								end_of_round = true,
-								card_effects = effects,
-							})
-							if next(eval) then
-								for h = 1, eval.jokers.repetitions do
-									reps[#reps + 1] = eval
-								end
-							end
-						end
-					end
-
-					for ii = 1, #effects do
-						--if this effect came from a joker
-						if effects[ii].card then
-							G.E_MANAGER:add_event(Event({
-								trigger = "immediate",
-								func = function()
-									effects[ii].card:juice_up(0.7)
-									return true
-								end,
-							}))
-						end
-
-						--If dollars
-						if effects[ii].h_dollars then
-							ease_dollars(effects[ii].h_dollars)
-							card_eval_status_text(G.hand.cards[i], "dollars", effects[ii].h_dollars, percent)
-						end
-
-						--Any extras
-						if effects[ii].extra then
-							card_eval_status_text(G.hand.cards[i], "extra", nil, percent, nil, effects[ii].extra)
-						end
-					end
-					j = j + 1
-				end
+			if G.GAME.round_resets.temp_handsize then
+				G.hand:change_size(-G.GAME.round_resets.temp_handsize)
+				G.GAME.round_resets.temp_handsize = nil
 			end
-			delay(0.3)
-
-			G.FUNCS.draw_from_hand_to_discard()
-			if G.GAME.blind:get_type() == "Boss" then
-				G.GAME.voucher_restock = nil
-				if
-					G.GAME.modifiers.set_eternal_ante
-					and (G.GAME.round_resets.ante == G.GAME.modifiers.set_eternal_ante)
-				then
-					for k, v in ipairs(G.jokers.cards) do
-						v:set_eternal(true)
-					end
-				end
-				if
-					G.GAME.modifiers.set_joker_slots_ante
-					and (G.GAME.round_resets.ante == G.GAME.modifiers.set_joker_slots_ante)
-				then
-					G.jokers.config.card_limit = 0
-				end
-				delay(0.4)
-				ease_ante(1)
-				delay(0.4)
-				check_for_unlock({ type = "ante_up", ante = G.GAME.round_resets.ante + 1 })
+			if G.GAME.round_resets.temp_reroll_cost then
+				G.GAME.round_resets.temp_reroll_cost = nil
+				calculate_reroll_cost(true)
 			end
-			G.FUNCS.draw_from_discard_to_deck()
-			G.E_MANAGER:add_event(Event({
-				trigger = "after",
-				delay = 0.3,
-				func = function()
-					G.STATE = G.STATES.ROUND_EVAL
-					G.STATE_COMPLETE = false
 
-					if
-						G.GAME.round_resets.blind_states.Small ~= "Defeated"
-						and G.GAME.round_resets.blind_states.Small ~= "Skipped"
-					then
-						G.GAME.round_resets.blind_states.Small = "Defeated"
-					elseif
-						G.GAME.round_resets.blind_states.Big ~= "Defeated"
-						and G.GAME.round_resets.blind_states.Big ~= "Skipped"
-					then
-						G.GAME.round_resets.blind_states.Big = "Defeated"
-					else
-						G.GAME.current_round.voucher = get_next_voucher_key()
-						G.GAME.round_resets.blind_states.Boss = "Defeated"
-						for k, v in ipairs(G.playing_cards) do
-							v.ability.played_this_ante = nil
-						end
-					end
-
-					if G.GAME.round_resets.temp_handsize then
-						G.hand:change_size(-G.GAME.round_resets.temp_handsize)
-						G.GAME.round_resets.temp_handsize = nil
-					end
-					if G.GAME.round_resets.temp_reroll_cost then
-						G.GAME.round_resets.temp_reroll_cost = nil
-						calculate_reroll_cost(true)
-					end
-
-					reset_idol_card()
-					reset_mail_rank()
-					reset_ancient_card()
-					reset_castle_card()
-					for k, v in ipairs(G.playing_cards) do
-						v.ability.discarded = nil
-						v.ability.forced_selection = nil
-					end
-					return true
-				end,
-			}))
+			reset_idol_card()
+			reset_mail_rank()
+			reset_ancient_card()
+			reset_castle_card()
+			for k, v in ipairs(G.playing_cards) do
+				v.ability.discarded = nil
+				v.ability.forced_selection = nil
+			end
 			return true
 		end,
 	}))
+	return true
 end
 
 local start_run_ref = Game.start_run
