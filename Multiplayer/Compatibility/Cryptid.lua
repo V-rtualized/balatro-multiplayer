24a713ba83868ef4947584b144ec4e574715568b
Connor Mills <connor@virtualized.dev>
2025-01-12 00:37:13 -0800

    Fixed certain cryptid cards creating banned cards

--- Multiplayer/Compatibility/Cryptid.lua
+++ Multiplayer/Compatibility/Cryptid.lua
@@ -3,6 +3,7 @@ if SMODS.Mods["Cryptid"] and SMODS.Mods["Cryptid"].can_load then
 	G.MULTIPLAYER.DECK.ban_card("j_cry_fleshpanopticon")
 	G.MULTIPLAYER.DECK.ban_card("j_cry_candy_sticks")
 	G.MULTIPLAYER.DECK.ban_card("j_cry_redeo")
+	G.MULTIPLAYER.DECK.ban_card("j_cry_chocolate_dice")
 	G.MULTIPLAYER.DECK.ban_card("v_cry_asteroglyph")
 	G.MULTIPLAYER.DECK.ban_card("c_cry_semicolon")
 	G.MULTIPLAYER.DECK.ban_card("c_cry_crash")
@@ -27,5 +28,28 @@ if SMODS.Mods["Cryptid"] and SMODS.Mods["Cryptid"].can_load then
 
 	function wheel_of_fortune_the_title_card()
 		return true
+	end
+
+	local get_random_consumable_ref = get_random_consumable
+	function get_random_consumable(seed, excluded_flags, banned_card, pool, no_undiscovered)
+		if not G.LOBBY.code then
+			return get_random_consumable_ref(seed, excluded_flags, banned_card, pool, no_undiscovered)
+		end
+		local tries = 5
+		while true do
+			local card = get_random_consumable_ref(seed, excluded_flags, banned_card, pool, no_undiscovered)
+			for _, banned in ipairs(G.MULTIPLAYER.DECK.BANNED_CARDS) do
+				if card.key == banned.id then
+					sendWarnMessage("Attempted to create banned card: " .. card.key .. ", trying again", "MULTIPLAYER")
+					tries = tries - 1
+					if tries <= 0 then
+						sendWarnMessage("Attempted to create banned cards too many times, giving up.", "MULTIPLAYER")
+						return card -- Giving up
+					end
+					break
+				end
+			end
+			return card
+		end
 	end
 end
